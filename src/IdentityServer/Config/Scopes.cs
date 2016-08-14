99d5a7b73d5c5559e540641a78521aa243e7ba8c
Mickaël Derriey <mderriey@gmail.com>
2016-08-15 08:58:21 +1000

    add basic IdentityServer configuration

---
+++ src/IdentityServer/Config/Scopes.cs
@@ -0,0 +1,17 @@
+﻿namespace IdentityServer.Config
+{
+    using IdentityServer3.Core.Models;
+    using System.Collections.Generic;
+
+    public static class Scopes
+    {
+        public static List<Scope> Get()
+        {
+            return new List<Scope>
+            {
+                StandardScopes.OpenId,
+                StandardScopes.Profile
+            };
+        }
+    }
+}
